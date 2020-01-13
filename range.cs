e2e4767a2bd3ee4ad91933043483f967b2f6abd8
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-13 13:02:15 -0500

    find data types range in the console

---
+++ range.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Variables
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+      Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+
+    }
+  }
+}
