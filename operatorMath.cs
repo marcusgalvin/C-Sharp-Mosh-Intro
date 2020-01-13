a2809f25371ae708ac050275009bb87e3f441123
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-13 13:35:28 -0500

    when printing to console c# follows the PEMDAS method

---
+++ operatorMath.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Variables
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      var a = 1;
+      var b = 2;
+      var c = 3;
+
+      Console.WriteLine(a + b * c);
+
+
+    }
+  }
+}
