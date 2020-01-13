cc448ca56a0fcf851f5b079c44ca122d45a7b70b
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-13 13:32:52 -0500

    quick example of casting numbers into a float

---
+++ casting.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Variables
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      var a = 10;
+      var b = 3;
+
+      Console.WriteLine((float)a / (float)b);
+
+
+    }
+  }
+}
