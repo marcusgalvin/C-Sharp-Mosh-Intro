77c3379096fb7e7f653aa0c1318cea424ccc3d13
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-17 14:44:18 -0500

    foreach and forloops

---
+++ loops.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Loops
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      //var name = "Marcus Galvin";
+
+      //for (var i = 0; i < name.Length; i++)
+      //{
+      //    Console.WriteLine(name[i]);
+      //}
+
+      //foreach (var character in name)
+      //{
+      //    Console.WriteLine(character);
+      //}
+
+
+
+      var numbers = new int[] { 1, 2, 3, 4 };
+
+      foreach (var number in numbers)
+      {
+        Console.WriteLine(number);
+      }
+    }
+
+
+  }
+}
