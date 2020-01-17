1ba9c0d516d53fe0091f543a4b9018e47fd4e54d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-17 14:54:30 -0500

    break and continue methods

---
+++ whileLoops.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace WhileLoops
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      //for (var i = 1; i <= 10; i++)
+      //{
+      //    if (i%2 == 0)
+      //    {
+      //        Console.WriteLine(i);
+      //    }
+      //}
+
+
+      //var i = 0;
+      //while (i <= 10)
+      //{
+      //    if (i % 2 == 0)
+      //        Console.WriteLine(i);
+      //    i++;
+      //}
+
+
+      //use a while loop when you dont know ahead of time how many times you want to iterate, no counter
+
+      while (true)
+      {
+        Console.Write("type your name: ");
+        var input = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(input))
+          break;
+
+        Console.WriteLine("@echo: " + input);
+      }
+
+
+
+
+
+    }
+  }
+}
